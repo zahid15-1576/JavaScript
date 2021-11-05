e5ca0e18ad81f5d2a47e97b100d15b520331c150
Md. Zahid Hassan <zahid15-1576@diu.edu.bd>
2021-11-06 01:37:47 +0600

    result

---
+++ result.js
@@ -0,0 +1,25 @@
+var result= 37;
+if(result<=100 && result>=80 ){
+    console.log("A+");
+}
+else if(result<=79 && result>=70 ){
+    console.log("A");
+}
+else if(result<=69 && result>=60 ){
+    console.log("A-");
+}
+else if(result<=59 && result>=50 ){
+    console.log("B");
+}
+else if(result<=49 && result>=40 ){
+    console.log("C");
+}
+else if(result<=39 && result>=33 ){
+    console.log("D");
+}
+else if(result<=32 && result>=0 ){
+    console.log("F");
+}
+else{
+    console.log("invalid Input")
+}
