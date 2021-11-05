01c27a5bd0a9680b26699f99d4db0f515d440b92
Md. Zahid Hassan <zahid15-1576@diu.edu.bd>
2021-11-06 02:17:07 +0600

    object

---
+++ object.js
@@ -0,0 +1,29 @@
+var rohim={
+
+    name:"Rohim Mia",
+    age: 25,
+    address: "Dhaka",
+    job: "Job holder"
+}
+var rakib={
+
+    name:"Rakib Mia",
+    age: 23,
+    address: "Dinajpur",
+    job: "Job holder"
+}
+var Omi={
+
+    name:"Omi Mia",
+    age: 15,
+    address: "Dhaka",
+    job: "Job "
+}
+var rasel={
+
+    name:"Rasel Mia",
+    age: 33,
+    address: "Khulna",
+    job: "Job holder"
+}
+console.log(rohim, rakib, Omi, rasel);
