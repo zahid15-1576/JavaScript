d3474e0a04fa48a22e4a70e1c1c7a1a8d229b899
Md. Zahid Hassan <zahid15-1576@diu.edu.bd>
2021-11-07 07:06:19 +0600

    Function

---
+++ function.js
@@ -0,0 +1,27 @@
+function test(){
+console.log("Hi!! My Name is zahid");
+}
+
+console.log(test());
+
+
+function sum(a,b){
+var add=a+b;
+return add;
+}
+
+console.log(sum(10,20));
+
+function sub(a,b){
+    var multi=a*b;
+    return multi;
+}
+
+console.log(sub(5,5));
+
+
+function myInfo(name, age){
+    console.log("my Name is "+name+ " and my age is "+age);
+}
+
+console.log(myInfo( 'hasan', 32));
