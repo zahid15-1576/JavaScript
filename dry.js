962b863976b68b6024e66278782e5ec6cb6619a8
Md. Zahid Hassan <zahid15-1576@diu.edu.bd>
2021-11-07 07:16:59 +0600

    dry

---
+++ dry.js
@@ -0,0 +1,19 @@
+function addTwoNum(x, y) {
+    var sum = x + y;
+    console.log('Sum of ' + x + ' and ' + y + ' is: ' + sum);
+ }
+console.log(addTwoNum(5, 10));
+console.log(addTwoNum(20, 30));
+console.log(addTwoNum(40, 50));
+console.log(addTwoNum(60, 70));
+console.log(addTwoNum(80, 90));
+
+
+function welcomeMsg(name) {
+    console.log('Welcome Mr. ' + name);
+ }
+
+console.log(welcomeMsg('Zonayed'));
+console.log(welcomeMsg('Ramazan'));
+console.log(welcomeMsg('Sagar'));
+console.log(welcomeMsg('Niraj'));
