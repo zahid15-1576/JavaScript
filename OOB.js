3fa0882a24a0a42ba804e3b8906e87a4272c7be6
Md. Zahid Hassan <zahid15-1576@diu.edu.bd>
2021-11-08 23:10:13 +0600

    Cons

--- OOB.js
+++ OOB.js
@@ -1,14 +1,17 @@
-var a = 10, b = 20;
-
-console.log('Before swap: Value of a: ' + a + ' and value of b: ' + b);
-
-function swap(a, b) {
-   console.log('Before Swap inside function: Value of a: ' + a + ' and value of b: ' + b);
-   var temp = a;
-   a = b;
-   b = temp;
-   console.log('After Swap inside function: Value of a: ' + a + ' and value of b: ' + b);
-}
-
-swap(a, b);
-console.log('After swap: Value of a: ' + a + ' and value of b: ' + b);
+var obj = {
+    a: 10,
+    b: 20
+ };
+ 
+ console.log('Before swap: Value of a: ' + obj.a + ' and value of b: ' + obj.b);
+ 
+ function swap(x) {
+    console.log('Before Swap inside function: Value of a: ' + x.a + ' and value of b: ' + x.b);
+    var temp = x.a;
+    x.a = x.b;
+    x.b = temp;
+    console.log('After Swap inside function: Value of a: ' + x.a + ' and value of b: ' + x.b);
+ }
+ 
+ swap(obj);
+ console.log('After swap: Value of a: ' + obj.a + ' and value of b: ' + obj.b);
---
+++ cn.js
@@ -0,0 +1,30 @@
+var samir = { 
+    name: 'Samir Hossain',
+    age: 22,
+    job: 'Student'
+ }
+ 
+ var kinan = { 
+    name: 'Kinan Hossain',
+    age: 29,
+    job: 'Businessman'
+ }
+ 
+ var jamil = { 
+    name: 'Jemil Hossain',
+    age: 18,
+    job: 'Driver'
+ }
+ 
+ var zawad = { 
+    name: 'Zawad Ahmed',
+    age: 1,
+    job: 'Child'
+ }
+ 
+ var zonayed = { 
+    name: 'Zonayed Ahmed',
+    age: 21,
+    job: 'Developer'
+ }
+ 
