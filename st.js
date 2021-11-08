f86f737c76ce0165dc36d361c44401c36231e7f1
Md. Zahid Hassan <zahid15-1576@diu.edu.bd>
2021-11-08 23:37:54 +0600

    st

---
+++ st.js
@@ -0,0 +1,17 @@
+'use strict'
+
+var name = 'Zonayed Ahmed';
+var age = 10;
+
+address = 'Cumilla';
+
+console.log('My name is ' + name + ' and I am ' + age + ' years old!');
+
+function aFunc() {
+   job = 'Student';
+   console.log('A Function with Strict Mode Enabled');
+}
+
+aFunc();
+
+console.log('I am a ' + job + ' And I am from ' + address);
