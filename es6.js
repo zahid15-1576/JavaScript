6b20319597ef7e0d9863753c83695ef54af39eb6
Md. Zahid Hassan <zahid15-1576@diu.edu.bd>
2021-11-09 08:58:20 +0600

    es6

--- es6.js
+++ es6.js
@@ -2,3 +2,9 @@ var a = 'Zonayed Ahmed';
 console.log('My name is: ' + a);
 a = 'Zawad Ahmed';
 console.log('Now the name is: ' + a);
+
+
+let a6 = 'Zonayed Ahmed';
+console.log('My name is: ' + a6);
+a6 = 'Zawad Ahmed';
+console.log('Now the name is: ' + a6);
