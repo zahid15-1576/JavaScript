2fd1827b6f292aa9befa3fa8a128ac2486c75f6f
Md. Zahid Hassan <zahid15-1576@diu.edu.bd>
2021-11-16 23:36:52 +0600

    class

---
+++ cl.js
@@ -0,0 +1,49 @@
+class PersonClassDemo {
+    constructor(name, age, job) {
+       this.name = name;
+       this.age = age;
+       this.job = job;
+       this.dateOfBirth = () => {
+          console.log(`${this.name} is born in ${2018 - this.age}`);
+       }
+    }
+ }
+
+ class PersonClassDemo {
+    constructor(name, age, job) {
+       this.name = name;
+       this.age = age;
+       this.job = job;
+       this.dateOfBirth = () => {
+          console.log(`${this.name} is born in ${2018 - this.age}`);
+       }
+    }
+ }
+
+ class PersonClassDemo {
+    constructor(name, age, job) {
+       this.name = name;
+       this.age = age;
+       this.job = job;
+       this.dateOfBirth = () => {
+          console.log(`${this.name} is born in ${2018 - this.age}`);
+       }
+    }
+ }
+
+ class TeacherClassDemo {
+    constructor(name, age, job, dateOfBirth, subject) {
+       this.name = name;
+       this.age = age;
+       this.job = job;
+       this.subject = subject;  
+       this.dateOfBirth = dateOfBirth;
+    }
+ }
+ class PersonClass {
+    constructor(name, age, job) {
+       this.name = name;
+       this.age = age;
+       this.job = job;
+    }
+ } 
